4518b21f9e60dd390c11189566971a5caf31918a
vagrant <vagrant@rails-dev-box>
2014-10-12 19:24:01 +0000

    First commit for doit app

---
+++ README.md
@@ -0,0 +1,4 @@
+== README 
+   Doit
+
+This app allows you to make todo lists among other things.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Doit::Application.config.secret_key_base = '2f890524c9d377e03ff74df700c17595f130e3d4654c27233170bc2ef78fa8c0337a038b8a2fb169279ace8712bca252562293469e3c8e214b39fd7905322e8a'
